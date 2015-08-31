7b70eade5a4af1e470da4379411477eda617b7c2
Gabe Hernandez <ghernandez345@gmail.com>
2015-08-30 22:15:58 -0500

    Docs for array utils

--- assets/js/utils/arrayUtils.js
+++ assets/js/utils/arrayUtils.js
@@ -1,18 +1,31 @@
 /**
- * Shuffle is a utility that handles the shuffling of the elements
+ * These are utility that handles the shuffling of the elements
  * in an array. It is from the Fisher-Yates shuffle algorithm.
  */
 
 'use strict';
 
+/**
+ * Will swap two elements in an array.
+ * @param  {Array} array     - Array whos items we want to swap.
+ * @param  {Number} element1 - First element we'd like to swap.
+ * @param  {Number} element2 - Second element to swap with the first element.
+ */
 export function swapElements (array, element1, element2) {
   var index1 = array.indexOf(element1);
   var index2 = array.indexOf(element2);
-  
+
+  // Swap elements after finding their indexes.
   array[index1] = element2;
   array[index2] = element1;
 }
 
+/**
+ * Handles the shuffling of the elements in an array.
+ * It is from the Fisher-Yates shuffle algorithm.
+ * @param  {Array} array - Array to shuffle.
+ * @return {Array}       - The shuffled array.
+ */
 export function shuffle (array) {
   let currentIndex = array.length;
   let temporaryValue;
